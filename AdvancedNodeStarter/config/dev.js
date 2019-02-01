83bdeb893292037d94223068b6fa864ed38839a8
程鹏 <chengpeng557@pingan.com.cn>
2019-02-01 17:52:36 +0800

    self config

--- AdvancedNodeStarter/config/dev.js
+++ AdvancedNodeStarter/config/dev.js
@@ -1,7 +1,10 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+    // '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+    '577123701647-pom50j6p6i1pgtgviehrmi58dh218a5s.apps.googleusercontent.com',
+  // googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  googleClientSecret: '0Okmdcx2ozG-iN1WguDExtN1',
+  // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://chengandpeng:priest41581@ds119085.mlab.com:19085/blog_dev',
   cookieKey: '123123123'
 };
