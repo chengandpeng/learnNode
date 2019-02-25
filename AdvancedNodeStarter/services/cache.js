cf9d4ffe367b9f92c22251ab04f8e5113048a2dd
程鹏 <chengandpeng@gmail.com>
2019-02-25 19:07:12 +0800

    update

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,10 +1,35 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
+mongoose.Query.prototype.exec = async function () {
     console.log('IM ABOUT TO RUN A QUERY');
-    console.log(this.getQuery());
     
-    return exec.apply(this, arguments);
+    const key = JSON.stringify(
+        Object.assign({}, this.getQuery(), {
+            collection: this.mongooseCollection.name
+        })
+    );
+
+    const cacheValue = await client.get(key);
+
+    // 'key' in redis
+    if (cacheValue) {
+        console.log(this.getQuery());
+        console.log(cacheValue);
+        return mongoose.set(this.getQuery(), JSON.parse(cacheValue));
+        // return new mongoose.SchemaType()
+        // return Promise.resolve(JSON.parse(cacheValue));
+    }
+
+    // 'key' not in redis
+    const result = await exec.apply(this, arguments);
+    client.set(key, JSON.stringify(result));
+    
+    return result;
 }
